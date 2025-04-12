73ea40c3f211c673d4cbd678b4cbf1b8e8d50740
urvashi <urviradadiya20@gmail.com>
2025-04-12 14:29:44 +0530

    - Change database configuration.

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -82,16 +82,12 @@ WSGI_APPLICATION = "finance_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME"),
-#         "USER": config("DB_USER"),
-#         "HOST": config("DB_HOST"),
-#         "PASSWORD": config("DB_PASSWORD"),
-#         "PORT": config("DB_PORT")
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres"
+    }
+}
 
 DATABASES['default'] = dj_database_url.parse(config("DB_URL"))
 
